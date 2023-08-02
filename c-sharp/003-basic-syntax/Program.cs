52b1082ec2683901fd8aa019a707a78c4f95fdf2
Saad Khan <khan.ssjc@gmail.com>
2023-08-02 18:57:17 -0400

    c-sharp/ var declaration and math operators`

--- c-sharp/003-basic-syntax/Program.cs
+++ c-sharp/003-basic-syntax/Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Basic Syntax of c-sharp");
+
+// basic var declaration, assignment, and operation
+int subTotal;
+subTotal = 10;
+
+int salesTax = 13;
+string item = "10k bananas";
+
+Console.WriteLine("I just bought {0} for {1}", item, subTotal + salesTax );
