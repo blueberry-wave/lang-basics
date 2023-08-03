047667864c9a705117731de54db27e41b46c85dd
Saad Khan <khan.ssjc@gmail.com>
2023-08-03 17:48:17 -0400

    c-sharp/ string data types
    Introduction to string literals, raw string literals, verbatim string
    literals, and interpolated string literals.

--- c-sharp/003-basic-syntax/Program.cs
+++ c-sharp/003-basic-syntax/Program.cs
@@ -17,3 +17,38 @@ Console.WriteLine("I just bought {0} for {1}", item, subTotal /* in-line comment
 	a multi-line
 	comment.
 */
+
+double heightInMeters = 1.78;
+Console.WriteLine($"The variable {nameof(heightInMeters)} has the value {heightInMeters}");
+
+char letter = 'A';
+char digit = '1';
+
+string firstName = "Saad";
+string lastName = "Khan";
+
+string horizontalLine = new('-', count: 74);
+string grinningEmoji = char.ConvertFromUtf32(0x1F600);
+
+Console.WriteLine(grinningEmoji);
+
+string fullNameWithTab = "Saad\tKhan";
+// string filePath = "C:\laptop\lenovo\x1";
+string filePath2 = @"C:\laptop\lenovo\x1";
+
+string xmlSample = """
+		<person age="50">
+		<first_name>Saad></first_name>
+		</person>
+		""";
+var person = new { FirstName = "Saad", Age = 25};
+
+string json = $$"""
+	{
+		"first_name" : "{{person.FirstName}}",
+		"age": {{person.Age}};
+		"calculation": "{{{1 + 2}}}"
+	}
+""";
+
+Console.WriteLine(json);
