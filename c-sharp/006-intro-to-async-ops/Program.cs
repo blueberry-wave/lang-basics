ad236214507296c98c3ea8a1a63836a0fa06e2ca
Saad Khan <khan.ssjc@gmail.com>
2023-08-04 17:48:42 -0400

    c-sharp/ async-await http response cycle
    Use async-await to have a non-blocking operationg for an http request.

--- c-sharp/006-intro-to-async-ops/Program.cs
+++ c-sharp/006-intro-to-async-ops/Program.cs
@@ -1,2 +1,7 @@
-﻿Console.WriteLine("006-intro-to-async-await-ops");
+﻿WriteLine("006-intro-to-async-await-ops");
 
+HttpClient client = new();
+
+HttpResponseMessage res = await client.GetAsync("http://www.apple.com/");
+
+WriteLine("Apple's home page hsa {0:N0} bytes", res.Content.Headers.ContentLength);
