c15c75d93fb7ccbf3f04ab1df15128cabfdea1d2
Saad Khan <khan.ssjc@gmail.com>
2023-08-04 18:02:02 -0400

    c-sharp/ 007-control-flow init
    Initialize a console application to explore control flow in .NET.

---
+++ c-sharp/007-control-flow/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+WriteLine("Hello, World!");
+
+int x = 5;
+int y = 3;
+int resAdd = x + y;
+int resMult = x * y;
+
+int postIncr = x ++;
+int preIncr = ++ x;
+
+Type typeOfInt = typeof(int);
+string nameOfInt = nameof(x);
+int byteSizeOfInt = sizeof(int);
+
+
