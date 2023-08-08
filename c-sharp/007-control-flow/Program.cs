07b335ab27dc2047ca29d7fabca3a3ba1b332e09
Saad Khan <khan.ssjc@gmail.com>
2023-08-07 22:19:31 -0400

    c-sharp/ logical operators

--- c-sharp/007-control-flow/Program.cs
+++ c-sharp/007-control-flow/Program.cs
@@ -1,8 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+
+// See https://aka.ms/new-console-template for more information
 WriteLine("Hello, World!");
 
-int x = 5;
-int y = 3;
+int x = 2;
+int y = 8;
 int resAdd = x + y;
 int resMult = x * y;
 
@@ -13,4 +15,33 @@ Type typeOfInt = typeof(int);
 string nameOfInt = nameof(x);
 int byteSizeOfInt = sizeof(int);
 
+x = 10;
+y = 6;
+WriteLine($"x = {x}");
+WriteLine($"y = {y}");
+WriteLine($"x & y = {x & y}");
+WriteLine($"x | y = {x | y}");
+WriteLine($"x ^ y = {x ^ y}");
+WriteLine($"x  =     | {ToBinaryString(x)}");
 
+
+bool a = true;
+bool b = false;
+
+WriteLine($"AND\t| a\t|b\t");
+WriteLine($"a\t|{ a & a, -5}");
+
+
+
+WriteLine($"a && DoStuf()=  {a && DoStuff()}");
+
+WriteLine($"b && DoStuf()=  {b && DoStuff()}");
+
+static string ToBinaryString(int val) {
+    return Convert.ToString(val, toBase: 2).PadLeft(8, '0');
+}
+
+static bool DoStuff() {
+    WriteLine("I am doing stuff");
+    return true;
+}
