2b336c1ef9f0158a726b314a9ba44036a1d791e8
Saad Khan <khan.ssjc@gmail.com>
2023-08-08 17:44:47 -0400

    c-sharp/ introduction to selection statements

--- c-sharp/007-control-flow/Program.cs
+++ c-sharp/007-control-flow/Program.cs
@@ -55,3 +55,126 @@ static bool DoStuff() {
 
 WriteLine($"2 << 3 {2 << 3}");
 WriteLine($"8 >> 3 {8 >> 3}");
+
+
+WriteLine("Control Flow 1");
+
+if (true) {
+    WriteLine("Flow 1 -true\n");
+}
+
+WriteLine("Control Flow 2");
+if (false) {
+    WriteLine("Flow 2 - true\n");
+} else if(true) {
+    WriteLine("Flow 2 - false\n");
+} 
+
+WriteLine("Control Flow 3");
+if (false) {
+    WriteLine("Flow 3- true\n");
+} else if(false) {
+    WriteLine("Flow 3 - false\n");
+} else {
+    WriteLine("Flow 3 - else");
+}
+
+
+object o = "3";
+int j = 4;
+
+if (o is int i) {
+    WriteLine($" {i} * {j} = {i * j}");
+} else {
+    WriteLine("o is not an int, cannot multiply");
+}
+
+object o2 = 10;
+if (o2 is int k) {
+    WriteLine($" o2: {k} * {j} = {k * j}");
+} else {
+    WriteLine("o2 is not an int, cannot multiply");
+}
+
+int randNum = Random.Shared.Next(1,5);
+WriteLine($"randNum is {randNum}");
+
+switch (randNum) {
+    case 1:
+        WriteLine($"case 1");
+        break;
+    case 2:
+        WriteLine("case 2");
+        break;
+    case 3:
+    case 4:
+        WriteLine("multi case, 3,4");
+        break;
+    default:
+        WriteLine("default case");
+        break;
+}
+
+Animal?[] animalArr = new Animal?[] {
+    new Cat {
+        Name= "Thoor",
+        Born= new(year: 2019, month: 07, day: 16),
+        Legs = 4,
+        IsDomestic = true
+        },
+    new Cat {
+        Name = "Palo",
+        Born = new(year: 2019, month: 2, day: 16),
+        Legs = 4,
+        IsDomestic = true
+    },
+    new Spider {
+        Name = "Miss Spider",
+        Born = new(year: 2010, month: 04, day: 4),
+        Legs = 8,
+        IsVenomous = true
+    },
+    new Spider {
+        Name = "Fury Cutter",
+        Born = DateTime.Today,
+        IsVenomous = false
+    },
+    null
+};
+
+foreach (Animal? animal in animalArr) {
+    string message;
+
+    switch (animal) {
+
+        case Cat FourLeggedCat when FourLeggedCat.Legs == 4:
+            WriteLine($"{FourLeggedCat.Name} has 4 legs!");
+            break;
+        default:
+            WriteLine($"{animal.Name} is a {animal.GetType().Name}");
+            break;
+        case Cat WildCat when WildCat.IsDomestic == false :
+            WriteLine($"{WildCat.Name} is a wild cat");
+            break; 
+        case null:
+            WriteLine("animal is null");
+            break;
+    }
+}
+
+
+class Animal {
+    public string? Name;
+    public DateTime Born;
+    public byte Legs;
+}
+
+class Cat : Animal {
+    public bool IsDomestic;
+}
+
+class Spider : Animal {
+    public bool IsVenomous;
+}
+
+
