0fd8c966edfd81bcc9b808c8fdf03bfb78c2de78
Saad Khan <khan.ssjc@gmail.com>
2023-08-14 18:32:39 -0400

    c-sharp/ exception handling with try-catch

--- c-sharp/010-intro-to-exceptions/Program.cs
+++ c-sharp/010-intro-to-exceptions/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("010-intro-to-exceptions");
+WriteLine("010-intro-to-exceptions");
+
+Write("What is your age");
+string? input = ReadLine();
+
+try {
+	int age = int.Parse(input!);
+	WriteLine($"You are {age} years old.");
+} catch (FormatException ex) {
+	WriteLine("Error caught");
+	WriteLine(ex);
+}
+
+WriteLine("post try-catch block");
