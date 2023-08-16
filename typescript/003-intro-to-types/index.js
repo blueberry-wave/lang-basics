863f40b6eb2bc2ecb07d47d6e74ee423587ec41f
Saad Khan <khan.ssjc@gmail.com>
2023-08-16 17:48:49 -0400

    typescript/ intro-to-basic-types

---
+++ typescript/003-intro-to-types/index.js
@@ -0,0 +1,6 @@
+"use strict";
+console.log("003-intro-to-types");
+let myBool = true;
+let myNum = 1234;
+let myString = "string";
+let myStrArr = [];
---
+++ typescript/003-intro-to-types/index.ts
@@ -0,0 +1,8 @@
+
+console.log("003-intro-to-types");
+
+let myBool : boolean = true;
+let myNum : number = 1234;
+let myString : string = "string";
+let myStrArr: string[] = [];
+
