912159f105aabedf5e4d7f2ac9a4c64aa5ce0632
Saad Khan <khan.ssjc@gmail.com>
2023-08-21 19:11:18 -0400

    typescript/ introduction to interfaces

--- typescript/005-interfaces-class-modules/index.js
+++ typescript/005-interfaces-class-modules/index.js
@@ -1,2 +1,46 @@
 "use strict";
-console.log('005-typescript-interfaces');
+console.log('005-interfaces-classes-modules');
+let idObject = {
+    id: 2,
+    name: "this is a name"
+};
+let optionalObj = {
+    id: 1
+};
+let optionalObj2 = {
+    id: 2,
+    name: "optionalObj2"
+};
+let weakNoOverlap = {};
+function printNameOrVal(obj) {
+    if (`id` in obj) {
+        console.log(`obj.name : ${obj.name}`);
+    }
+    if (`descr` in obj) {
+        console.log(`obj.val: ${obj.val}`);
+    }
+}
+printNameOrVal({
+    id: 1,
+    name: "this is a name"
+});
+printNameOrVal({
+    descr: "descriptipn",
+    val: 2
+});
+function getProperty(key, val) {
+    console.log(`${key} = ${val[key]}`);
+}
+;
+getProperty("id", {
+    id: 1,
+    name: "firstName"
+});
+getProperty("name", {
+    id: 2,
+    name: "secondName"
+});
+getProperty("id", {
+    id: 3,
+    name: "thirdName"
+});
--- typescript/005-interfaces-class-modules/index.ts
+++ typescript/005-interfaces-class-modules/index.ts
@@ -1,2 +1,97 @@
 
 console.log('005-interfaces-classes-modules');
+
+interface IIdName {
+  id: number;
+  name: string;
+}
+
+let idObject: IIdName = {
+  id: 2,
+  name: "this is a name"
+}
+
+interface IOptional {
+  id: number;
+  name?: string
+}
+
+let optionalObj: IOptional = {
+  id: 1
+}
+
+let optionalObj2: IOptional = {
+  id: 2,
+  name: "optionalObj2"
+}
+
+interface WeakType {
+  id?: number;
+  name?: string
+}
+
+let weakNoOverlap: WeakType = {}
+
+interface myIdName {
+  id: number;
+  name: string;
+}
+
+interface IDescVal {
+  descr: string;
+  val: number;
+}
+
+function printNameOrVal (
+  obj: myIdName | IDescVal
+): void {
+  if (`id` in obj) {
+    console.log(`obj.name : ${obj.name}`);
+  }
+
+  if(`descr` in obj) {
+    console.log(`obj.val: ${obj.val}`)
+  }
+}
+
+printNameOrVal({
+  id: 1,
+  name:"this is a name"
+});
+
+printNameOrVal({
+  descr: "descriptipn",
+  val: 2
+})
+
+interface Person {
+  id: number;
+  name: string;
+}
+
+type PersonProPName = keyof Person;
+
+function getProperty(key: PersonProPName,val: Person) {
+  console.log(`${key} = ${val[key]}`);
+};
+
+getProperty(
+  "id", 
+  {
+    id: 1,
+    name: "firstName"
+  })
+
+getProperty(
+  "name", 
+  {
+    id: 2,
+    name: "secondName"
+  })
+
+getProperty(
+  "id", 
+  {
+    id: 3,
+    name: "thirdName"
+  })
