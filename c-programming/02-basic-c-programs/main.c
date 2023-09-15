d2f5510269e41428a87a7e8ff2218b1216abc9d7
Saad Khan <khan.ssjc@gmail.com>
2023-09-14 23:11:51 -0400

    c-programming/ introduction to variables

--- c-programming/02-basic-c-programs/main.c
+++ c-programming/02-basic-c-programs/main.c
@@ -1,8 +1,37 @@
 #include <stdio.h>
 
-int main (void) {
+int main(void) {
   char projectTitle[] = "02-basic-c-programs";
-  printf("%s", projectTitle);
+  printf("%s\n", projectTitle);
+
+  char myChar = 97;
+
+  printf("%c\n", myChar);
+  printf("%d\n", myChar);
+  printf("The size of a character objcet is %zi byte(s)\n", sizeof(myChar));
+
+  int x;
+  x = 10;
+
+  printf("Decimal: %d Octal: %o Hexadecimal: %X\n", x, x, x);
+
+  unsigned int y = 12345679u;
+
+  // exercises
+  /* multiline commenct
+  -
+  -
+  */
+
+  char cc = 'a';
+  int xx = 123;
+  float ff = 123.456f;
+  double dd = 789.101112;
+  
+  printf("%c\n", cc);  
+  printf("%d\n", xx);  
+  printf("%f\n", ff);  
+  printf("%f\n", dd);  
 
   return 0;
 }
