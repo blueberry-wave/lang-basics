d074379ed047b4c63c826fa535ef1210b7d688f3
Saad Khan <khan.ssjc@gmail.com>
2023-09-15 21:59:30 -0400

    c-programming/ assignment operators

--- c-programming/02-basic-c-programs/main.c
+++ c-programming/02-basic-c-programs/main.c
@@ -1,37 +1,15 @@
 #include <stdio.h>
 
 int main(void) {
-  char projectTitle[] = "02-basic-c-programs";
-  printf("%s\n", projectTitle);
-
-  char myChar = 97;
-
-  printf("%c\n", myChar);
-  printf("%d\n", myChar);
-  printf("The size of a character objcet is %zi byte(s)\n", sizeof(myChar));
-
   int x;
-  x = 10;
+  x = 123;
+  printf("%d", x);
 
-  printf("Decimal: %d Octal: %o Hexadecimal: %X\n", x, x, x);
-
-  unsigned int y = 12345679u;
-
-  // exercises
-  /* multiline commenct
-  -
-  -
-  */
-
-  char cc = 'a';
-  int xx = 123;
-  float ff = 123.456f;
-  double dd = 789.101112;
+  int y;
+  y = x;
+  printf("%d", y);
   
-  printf("%c\n", cc);  
-  printf("%d\n", xx);  
-  printf("%f\n", ff);  
-  printf("%f\n", dd);  
-
-  return 0;
+  char c;
+  c = 'A';
+  float f = 123.456f;
 }
